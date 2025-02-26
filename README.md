0d0d6e43f9a9e5b319491ba290f3f5ad08c2aaa1
Ajitesh-jam <ajitesh.jam@kgpian.iitkgp.ac.in>
2025-02-27 00:11:25 +0530

    added database

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gram-panchayat
 DBMS Project 
+
+
+``` \o output_file.txt ``` -> show the output in a file
+```\pset pager off``` -> show the output in one go in terminal
+```select * ,case when citizen_id>50 then 'citizen bada aadmi hai' else 'chota aadmi hai' end as kaisa_admi_hai from scheme_enrollments group by enrollment_id, kaisa_admi_hai order by kaisa_admi_hai;```
---
+++ database/village.sql
@@ -0,0 +1,17 @@
+create table village (
+	village_id INT Primary key ,
+	name VARCHAR(9),
+	census_data_id INT,
+	state VARCHAR(17),
+	pin_code INT
+);
+insert into village (village_id, name, census_data_id, state, pin_code) values (1, 'Phulera', 1, 'Madhya Pradesh', 3457173);
+insert into village (village_id, name, census_data_id, state, pin_code) values (2, 'Kharagput', 64, 'West Bengal', 3277600);
+insert into village (village_id, name, census_data_id, state, pin_code) values (3, 'Bikaner', 76, 'Rajasthan', 5413013);
+insert into village (village_id, name, census_data_id, state, pin_code) values (4, 'Jaipur', 42, 'Manipur', 8634196);
+insert into village (village_id, name, census_data_id, state, pin_code) values (5, 'Ajmer', 86, 'Tripura', 8012439);
+insert into village (village_id, name, census_data_id, state, pin_code) values (6, 'Dosa', 81, 'Rajasthan', 2120980);
+insert into village (village_id, name, census_data_id, state, pin_code) values (7, 'Badmer', 93, 'Telangana', 5085417);
+insert into village (village_id, name, census_data_id, state, pin_code) values (8, 'Bastar', 32, 'Nagaland', 5005862);
+insert into village (village_id, name, census_data_id, state, pin_code) values (9, 'Bhind', 97, 'Manipur', 4935078);
+insert into village (village_id, name, census_data_id, state, pin_code) values (10, 'Muraina', 87, 'Tripura', 9303288);

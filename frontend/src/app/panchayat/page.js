166ab0e8490bd9150930c1df5d6b934f0407628f
chadsaras <123252208+chadsaras@users.noreply.github.com>
2025-03-02 02:43:54 +0530

    Merge branch 'devlpoment' of https://github.com/Ajitesh-jam/gram-panchayat into devlpoment

--- frontend/src/app/panchayat/page.js
+++ frontend/src/app/panchayat/page.js
@@ -6,12 +6,13 @@ import useEmployees from "../../components/hooks/employee.zustand";
 import useGovernment from "@/src/components/hooks/government.zustand";
 import Layout from "../../components/layout/Layout";
 import Link from "next/link";
+import "./style.css"
 
 export default function Login() {
     const [role, setRole] = useState("");
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
-    
+
     const router = useRouter();
     const setEmployee = useEmployees((state) => state.setNewEmployee);
     const setGovt = useGovernment((state) => state.setNewGovernment);
@@ -31,18 +32,19 @@ export default function Login() {
             if (role === "Government Monitor") {
 
 
-                apiUrl = `/api/govt/get?govt_id=${id}&password=${password}`;}
+                apiUrl = `/api/govt/get?govt_id=${id}&password=${password}`;
+            }
             if (role === "Admin") apiUrl = `/api/admin/get?admin_id=${id}&password=${password}`;
 
             const response = await axios.get(apiUrl);
 
 
             if (role === "Employee") {
-                    setEmployee(response.data);
-                }
+                setEmployee(response.data);
+            }
             if (role === "Government Monitor") {
-                    
-               setGovt(response.data);
+
+                setGovt(response.data);
 
             }
 
@@ -50,7 +52,7 @@ export default function Login() {
             if (response.status === 200) {
                 const navigateTo =
                     role === "Employee" ? "/panchayat-employee" :
-                    role === "Government Monitor" ? "/govt-monitor" : "/admin";
+                        role === "Government Monitor" ? "/govt-monitor" : "/admin";
                 router.push(navigateTo);
             }
         } catch (error) {
@@ -79,11 +81,11 @@ export default function Login() {
                                 <div className="sec-title mb_50">
                                     <h2>Login</h2>
                                 </div>
-                                <div className="row clearfix">
-                                    <div className="col-lg-6 col-md-6 col-sm-12 form-group">
+                                <div className="row_clearfix">
+                                    <div className="form-group">
                                         <input type="text" placeholder="ID" onChange={(e) => setId(e.target.value)} required />
                                     </div>
-                                    <div className="col-lg-6 col-md-6 col-sm-12 form-group">
+                                    <div className="form-group">
                                         <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
                                     </div>
                                     <div className="col-lg-12 col-md-12 col-sm-12 form-group">
@@ -95,9 +97,12 @@ export default function Login() {
                                         </select>
                                     </div>
                                 </div>
-                                <button type="submit" className="theme-btn btn-one" onClick={login}><span>Login</span></button>
+                                <button type="submit" className="theme-btn btn-one" onClick={login}>Login</button>
                                 <p>Don't have an account? <Link href="/Employee-sign">Register</Link></p>
                             </div>
+                        </div>
+                        <div className="col-lg-4 col-md-12 col-sm-12 image-column">
+                            <figure className="image-box"><img src="assets/images/resource/citizen4.jpg" alt="" /></figure>
                         </div>
                     </div>
                 </div>
